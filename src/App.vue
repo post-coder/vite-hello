ac0b93928249018aab42425919c26ce56be9069d
post-coder <gabrielspanu.dev@gmail.com>
2024-02-28 11:58:20 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+
+// importare componente
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  components: {
+    AppTitle,
+  },
+
+  data() {
+    return  {
+      variabile: "valore"
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <AppTitle></AppTitle>
+
+
+</template>
+
+<style>
+
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'AppTitle',
+}
+</script>
+
+<template>
+    <h1>Titolo</h1>
+</template>
+
+<style>
+
+
+h1 {
+  color: red;
+}
+
+</style>
